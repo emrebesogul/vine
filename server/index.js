9cebd9efc22e84fef19fbb757d17a9a7fb7f1ab9
Konstantin Kläger <konstantin_klaeger@web.de>
2018-04-12 12:15:29 +0200

    Add mongo client to index.js

--- server/index.js
+++ server/index.js
@@ -5,11 +5,27 @@ const assert = require('assert');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-
-// create application/json parser
 const jsonParser = bodyParser.json();
-
 const database = require('./database');
 const url = 'mongodb://127.0.0.1:27017/vine';
 
-console.log("Hello")
+MongoClient.connect(url, function(err, client) {
+    if (err) {
+        console.log('Unable to connect to MongoDB');
+        throw err;
+    } else {
+        console.log("Successfully connected to MongoDB");
+        app.use(bodyParser.json());
+
+        /* Calls the method listProducts that returns all products to the react application */
+        app.get('/rest/product/list', (req, res) => {
+            database.listProducts(client.db('vine'), res);
+        });
+
+        /* Calls the method listSupplier that returns all supplier to the react application */
+        app.get('/rest/supplier/list', (req, res) => {
+            database.listProducts(client.db('vine'), res);
+        });
+    }        
+});
+
